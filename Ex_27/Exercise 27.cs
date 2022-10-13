4d1558943f101ee65976c1bbae38b7f76116ab44
Vadim <lyzhinv@gmail.com>
2022-10-13 18:11:15 +0300

    Add Exercise 27

---
+++ Ex_27/Exercise 27.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в числе {num} = {GetSum(num)}");
+
+int GetSum(int number)
+{
+    int sum = 0;
+    int size = Convert.ToString(number).Length;
+    for (int i = 0; i < size; i++)
+    {
+        int temp = number - number % 10;
+        sum = sum + (number - temp);
+        number = number / 10;
+    }
+
+    return sum;
+}
